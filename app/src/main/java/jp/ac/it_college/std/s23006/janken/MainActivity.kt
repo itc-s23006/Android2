a2c1f8c7219bd2417ebcef99ea307cae8df3b78a
itc-s23006 <s23006@std.it-college.ac.jp>
2024-10-02 09:47:10 +0900

    じゃんけんゲーム、画像とじゃんけんを合わせる。 コンポーサブル、完成まで。

--- app/src/main/java/jp/ac/it_college/std/s23006/janken/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/janken/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import jp.ac.it_college.std.s23006.janken.ui.theme.JankenTheme
 
@@ -44,4 +46,79 @@ fun GreetingPreview() {
     JankenTheme {
         Greeting("Android")
     }
+}
+@Composable
+fun PlayerView(modifier: Modifier = Modifier, hand: Int) {
+    if (hand == 0) {
+        Image(
+            painter = painterResource(id = R.drawable.gu),
+            contentDescription = null
+        )
+    } else if (hand == 1) {
+        Image(
+            painter = painterResource(id = R.drawable.choki),
+            contentDescription = null
+        )
+    } else if (hand == 2) {
+        Image(
+            painter = painterResource(id = R.drawable.pa),
+            contentDescription = null
+        )
+    }
+}
+@Preview
+@Composable
+private fun PlayerViewPreview0() {
+    PlayerView(hand = 0)
+}
+@Preview
+@Composable
+private fun PlayerViewPreview1() {
+    PlayerView(hand = 1)
+}
+@Preview
+@Composable
+private fun PlayerViewPreview2() {
+    PlayerView(hand = 2)
+}
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF00FF00,
+    widthDp = 100, heightDp = 200
+)
+@Composable
+private fun PlayerViewPreview() {
+    PlayerView(hand = 1)
+}
+@Composable
+fun ComputerView(modifier: Modifier = Modifier, comHand: Int) {
+    when (comHand) {
+        0 -> Image(
+            painter = painterResource(id = R.drawable.com_gu),
+            contentDescription = null
+        )
+        1 -> Image(
+            painter = painterResource(id = R.drawable.com_choki),
+            contentDescription = null
+        )
+        2 -> Image(
+            painter = painterResource(id = R.drawable.com_pa),
+            contentDescription = null
+        )
+    }
+}
+@Preview
+@Composable
+private fun ComputerViewPreview0() {
+    ComputerView(comHand = 0)
+}
+@Preview
+@Composable
+private fun ComputerViewPreview1() {
+    ComputerView(comHand = 1)
+}
+@Preview
+@Composable
+private fun ComputerViewPreview2() {
+    ComputerView(comHand = 2)
 }
