7a5b33b46ac1067661dc27275f1e596f1c38b9a0
itc-s23006 <s23006@std.it-college.ac.jp>
2024-10-09 09:51:39 +0900

    エラーが出たので、修正 ＆注文画面完成。

--- app/src/main/java/jp/ac/it_college/std/s23006/order/ui/OrderView.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/order/ui/OrderView.kt
@@ -52,9 +52,10 @@ import jp.ac.it_college.std.s23006.order.R
 import jp.ac.it_college.std.s23006.order.ui.theme.BaseColor
 import jp.ac.it_college.std.s23006.order.ui.theme.Orange400
 import jp.ac.it_college.std.s23006.order.ui.theme.Pink80
+
 @Composable
 fun OrderView(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     @DrawableRes imageRes: Int = R.drawable.classicbeef,
     onTapButton: () -> Unit = {}
 ) {
@@ -99,6 +100,7 @@ fun OrderView(
         )
     }
 }
+
 @Preview(
     device = "spec:width=400dp,height=1200dp", showBackground = true,
     backgroundColor = 0xff000000
@@ -114,168 +116,278 @@ private fun OrderViewPreview() {
 
 @Composable
 fun RadioButtonWithText(
-    @@ -142,4 +230,164 @@ fun SideMenuSection(modifier: Modifier = Modifier) {
-    @Composable
-    private fun SideMenuSectionPreview() {
-        SideMenuSection()
-
+    modifier: Modifier = Modifier,
+    text: String,
+    selected: Boolean,
+    onSelect: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .selectable(selected = selected, onClick = onSelect)
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick = null
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
+}
 
-    @Composable
-    fun SauceAmountSection(modifier: Modifier = Modifier) {
-        var sauceAmount by remember { mutableFloatStateOf(0f) }
-        Box(
+@Preview(showBackground = true)
+@Composable
+private fun RadioButtonWithTextPreview() {
+    RadioButtonWithText(
+        text = "ハンバーガー",
+        selected = true,
+    )
+}
+
+@Composable
+fun MainDishSection(modifier: Modifier = Modifier) {
+    var selectedDish by remember { mutableIntStateOf(R.string.hamburger) }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(BaseColor, shape = MaterialTheme.shapes.extraLarge)
+    ) {
+        Column(
             modifier = Modifier
+                .padding(16.dp)
                 .fillMaxWidth()
-                .background(
-                    color = BaseColor,
-                    shape = MaterialTheme.shapes.extraLarge
-                )
+                .selectableGroup()
         ) {
-            Column(
-                modifier = Modifier.padding(16.dp)
+            Text(
+                text = stringResource(id = R.string.choose_main),
+                style = MaterialTheme.typography.titleLarge
+            )
+            RadioButtonWithText(
+                text = stringResource(R.string.hamburger),
+                selected = selectedDish == R.string.hamburger,
+                onSelect = { selectedDish = R.string.hamburger }
+            )
+            RadioButtonWithText(
+                text = stringResource(R.string.cheeseburger),
+                selected = selectedDish == R.string.cheeseburger,
+                onSelect = { selectedDish = R.string.cheeseburger }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Preview(locale = "ja", showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun MainDishSectionPreview() {
+    MainDishSection()
+}
+
+@Composable
+fun SideMenuSection(modifier: Modifier = Modifier) {
+    var frenchFries by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = BaseColor,
+                shape = MaterialTheme.shapes.extraLarge
+            )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.choose_side),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Row(
+                modifier = Modifier.toggleable(
+                    value = frenchFries,
+                    onValueChange = { frenchFries = it }
+                )
             ) {
-                Text(
-                    text = stringResource(R.string.amount_sauce),
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Slider(
-                    value = sauceAmount,
-                    onValueChange = { sauceAmount = it },
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
+                Checkbox(
+                    checked = frenchFries,
+                    onCheckedChange = null
                 )
                 Text(
-                    text = when (sauceAmount) {
-                        in 0.0f..<0.3f -> stringResource(R.string.less)
-                        in 0.3f..0.7f -> stringResource(R.string.normal)
-                        else -> stringResource(R.string.more)
-                    },
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
+                    text = stringResource(id = R.string.french_fries),
+                    style = MaterialTheme.typography.bodyLarge
                 )
             }
         }
     }
+}
 
-    @Preview(showBackground = true, backgroundColor = 0xff000000)
-    @Preview(locale = "ja", showBackground = true, backgroundColor = 0xff000000)
-    @Composable
-    private fun SauceAmountSectionPreview() {
-        SauceAmountSection()
-    }
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Preview(showBackground = true, backgroundColor = 0xFF000000, locale = "ja")
+@Composable
+private fun SideMenuSectionPreview() {
+    SideMenuSection()
+}
 
-    @Composable
-    fun DrinkSelectionSection(modifier: Modifier = Modifier) {
-        var expanded by remember { mutableStateOf(false) }
-        var selectedDrink by remember { mutableIntStateOf(R.string.blank) }
-        val drinks = listOf(
-            R.string.iced_coffee,
-            R.string.iced_cafe_au_lait,
-            R.string.coke
-        )
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = BaseColor,
-                    shape = MaterialTheme.shapes.extraLarge
-                )
+@Composable
+fun SauceAmountSection(modifier: Modifier = Modifier) {
+    var sauceAmount by remember { mutableFloatStateOf(0f) }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = BaseColor,
+                shape = MaterialTheme.shapes.extraLarge
+            )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
-            Column(
+            Text(
+                text = stringResource(R.string.amount_sauce),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Slider(
+                value = sauceAmount,
+                onValueChange = { sauceAmount = it },
                 modifier = Modifier
                     .padding(16.dp)
+                    .fillMaxWidth()
+            )
+            Text(
+                text = when (sauceAmount) {
+                    in 0.0f..<0.3f -> stringResource(R.string.less)
+                    in 0.3f..0.7f -> stringResource(R.string.normal)
+                    else -> stringResource(R.string.more)
+                },
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xff000000)
+@Preview(locale = "ja", showBackground = true, backgroundColor = 0xff000000)
+@Composable
+private fun SauceAmountSectionPreview() {
+    SauceAmountSection()
+}
+
+@Composable
+fun DrinkSelectionSection(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedDrink by remember { mutableIntStateOf(R.string.blank) }
+    val drinks = listOf(
+        R.string.iced_coffee,
+        R.string.iced_cafe_au_lait,
+        R.string.coke
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = BaseColor,
+                shape = MaterialTheme.shapes.extraLarge
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.choose_your_drink),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = stringResource(R.string.choose_your_drink),
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Box(
+                OutlinedTextField(
+                    value = stringResource(selectedDrink),
+                    onValueChange = {},
                     modifier = Modifier
-                        .padding(16.dp)
                         .fillMaxWidth()
+                        .clickable { expanded = true },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowDropDown,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .clickable { expanded = true }
+                        )
+                    },
+                    label = {
+                        Text(text = stringResource(R.string.drink))
+                    },
+                    readOnly = true
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
                 ) {
-                    OutlinedTextField(
-                        value = stringResource(selectedDrink),
-                        onValueChange = {},
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { expanded = true },
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Filled.ArrowDropDown,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .clickable { expanded = true }
-                            )
-                        },
-                        label = {
-                            Text(text = stringResource(R.string.drink))
-                        },
-                        readOnly = true
-                    )
-                    DropdownMenu(
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false }
-                    ) {
-                        drinks.forEach { drink ->
-                            DropdownMenuItem(
-                                text = {
-                                    Text(text = stringResource(drink))
-                                },
-                                onClick = {
-                                    selectedDrink = drink
-                                    expanded = false
-                                }
-                            )
-                        }
+                    drinks.forEach { drink ->
+                        DropdownMenuItem(
+                            text = {
+                                Text(text = stringResource(drink))
+                            },
+                            onClick = {
+                                selectedDrink = drink
+                                expanded = false
+                            }
+                        )
                     }
                 }
             }
         }
     }
+}
 
-    @Preview(showBackground = true, backgroundColor = 0xff000000)
-    @Preview(showBackground = true, backgroundColor = 0xff000000, locale = "ja")
-    @Composable
-    private fun DrinkSelectionSectionPreview() {
-        DrinkSelectionSection()
-    }
+@Preview(showBackground = true, backgroundColor = 0xff000000)
+@Preview(showBackground = true, backgroundColor = 0xff000000, locale = "ja")
+@Composable
+private fun DrinkSelectionSectionPreview() {
+    DrinkSelectionSection()
+}
 
-    @Composable
-    fun OrderButtonSection(
-        modifier: Modifier = Modifier,
-        text: String,
-        onClick: () -> Unit = {}
-    ) {
-        val gradient = Brush.verticalGradient(
-            colors = listOf(Pink80, Orange400)
-        )
-        Button(
-            onClick = onClick,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent
-            ),
-            contentPadding = PaddingValues(),
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    brush = gradient,
-                    shape = MaterialTheme.shapes.extraLarge
-                )
-        ) {
-            Text(
-                text = text,
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                fontSize = 40.sp
+@Composable
+fun OrderButtonSection(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit = {}
+) {
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Pink80, Orange400)
+    )
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        contentPadding = PaddingValues(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                brush = gradient,
+                shape = MaterialTheme.shapes.extraLarge
             )
-        }
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp
+        )
     }
+}
 
-    @Preview(showBackground = true)
-    @Composable
-    private fun OrderButtonSectionPreview() {
-        OrderButtonSection(text = "注文する")
-    }
+@Preview(showBackground = true)
+@Composable
+private fun OrderButtonSectionPreview() {
+    OrderButtonSection(text = "注文する")
+}
