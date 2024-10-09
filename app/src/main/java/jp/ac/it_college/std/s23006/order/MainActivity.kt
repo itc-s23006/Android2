bb87ffe6d6501c7336420733fb2a231857856fda
itc-s23006 <s23006@std.it-college.ac.jp>
2024-10-09 14:56:44 +0900

    chap5、終了

--- app/src/main/java/jp/ac/it_college/std/s23006/order/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/order/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import jp.ac.it_college.std.s23006.order.ui.MainScreen
+import jp.ac.it_college.std.s23006.order.ui.theme.ForestGreen
 import jp.ac.it_college.std.s23006.order.ui.theme.OrderTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +24,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             OrderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        color = ForestGreen
+                        ) {
+                        MainScreen()
+                    }
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrderTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/jp/ac/it_college/std/s23006/order/ui/MainScreen.kt
@@ -0,0 +1,17 @@
+package jp.ac.it_college.std.s23006.order.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    var isOrderView by remember { mutableStateOf(false) }
+    if (isOrderView) {
+        OrderView(onTapButton = { isOrderView = false })
+    } else {
+        TopView(onTapButton = { isOrderView = true })
+    }
+}
