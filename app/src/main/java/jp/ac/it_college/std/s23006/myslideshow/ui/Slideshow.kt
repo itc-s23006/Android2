88a4c1cb23d6b24f75cfc948466b97fe365fb419
itc-s23006 <s23006@std.it-college.ac.jp>
2024-10-24 16:52:06 +0900

    教科書ｐ２３５の横画面に対応するまで。

---
+++ app/src/main/java/jp/ac/it_college/std/s23006/myslideshow/ui/Slideshow.kt
@@ -0,0 +1,62 @@
+package jp.ac.it_college.std.s23006.myslideshow.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.ac.it_college.std.s23006.myslideshow.R
+
+val resources = listOf(
+    R.drawable.slide00, R.drawable.slide01, R.drawable.slide02,
+    R.drawable.slide03, R.drawable.slide04, R.drawable.slide05,
+    R.drawable.slide06, R.drawable.slide07, R.drawable.slide08,
+    R.drawable.slide09
+)
+
+@Composable
+fun Slideshow(modifier: Modifier = Modifier) {
+    val pagerState = rememberPagerState(pageCount = { resources.size })
+    val config = LocalConfiguration.current
+
+    Scaffold(
+        topBar = {
+            if (config.orientation != Configuration.ORIENTATION_LANDSCAPE) {
+                TopBar()
+            }
+        },
+        bottomBar = {
+            if (config.orientation != Configuration.ORIENTATION_LANDSCAPE) {
+                BottomBar(pagerState = pagerState)
+            }
+        },
+    ) { padding ->
+            VerticalPager(
+                state = pagerState,
+                contentPadding = PaddingValues(
+                    vertical =
+                    if (config.orientation != Configuration.ORIENTATION_LANDSCAPE) 60.dp
+                    else 0.dp
+                ),
+                modifier = Modifier.padding(padding)
+            ) { page ->
+                PagerItem(
+                    imageRes = resources[page],
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+}
+
+@Preview
+@Composable
+private fun SlideshowPreview() {
+    Slideshow()
+}
