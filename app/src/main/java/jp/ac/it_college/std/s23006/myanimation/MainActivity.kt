02a13d0e2f3a8df36417bda7234d6f520b160afc
itc-s23006 <s23006@std.it-college.ac.jp>
2024-10-24 15:05:09 +0900

    chap9おわり

--- app/src/main/java/jp/ac/it_college/std/s23006/myanimation/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/myanimation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import jp.ac.it_college.std.s23006.myanimation.ui.TutorialPager
 import jp.ac.it_college.std.s23006.myanimation.ui.theme.MyAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAnimationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TutorialPager()
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
