5fcf94d4601b45743badfd5583927c6d017ce849
itc-s23006 <s23006@std.it-college.ac.jp>
2024-11-07 14:43:40 +0900

    教科書Ｐ３２０ 実行してみようまで。

--- app/src/main/java/jp/ac/it_college/std/s23006/mytodo/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/mytodo/MainActivity.kt
@@ -4,17 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import jp.ac.it_college.std.s23006.mytodo.data.Item
 import jp.ac.it_college.std.s23006.mytodo.ui.theme.MyTodoTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,24 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val coroutineScope = rememberCoroutineScope()
-                    val app = application as TodoApplication
-                    val db = app.container.itemsRepository
-                    val item = Item(
-                        title = "牛乳を買う", description = "2カートン", done = true
-                    )
-                    Row {
-                        Button(
-                            onClick = {
-                            coroutineScope.launch {
-                                db.insertItem(item)
-                            }
-                        }) {
-                            Text(text = "データをインポート")
-                        }
+                    TodoApp()
                     }
                 }
             }
         }
     }
-}
